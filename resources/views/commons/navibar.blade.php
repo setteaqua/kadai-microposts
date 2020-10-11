81dda2b1db5af0b148dee219e0ecd89f3bcc2c8e
setteaqua <mmg.service.account@gmail.com>
2020-10-11 08:08:29 +0000

    top page

---
+++ resources/views/commons/navibar.blade.php
@@ -0,0 +1,20 @@
+<hedader class="mb-4">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        {{--トップページへのリンク--}}
+        <a class="navbar-brand" href="/">Microposts</a>
+        
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collepse navbar-collapse" id="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav"></ul>
+                {{-- ユーザ登録ページへのリンク --}}
+                <li class="nav-item"><a href="#" class="nav-link">Signup</a></li>
+                {{-- ログインページへのリンク --}}
+                <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+            </ul>
+        </div>
+    </nav>
+</hedader>
