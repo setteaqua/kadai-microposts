d5d2ac21746ef6967d9194d0eeba6372c1033435
setteaqua <mmg.service.account@gmail.com>
2020-10-22 12:43:22 +0000

    follow,unfollow

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
             <aside class="col-sm-4">
                 <div class="card">
                     <div class="card-header">
-                        <h3 class="card-title">{{Auth::user()->naem }}</h3>
+                        <h3 class="card-title">{{Auth::user()->name }}</h3>
                     </div>
                     <div class="card-body">
                         {{--認証済みユーザのメールアドレスをもとにFravatarを取得して表示--}}
